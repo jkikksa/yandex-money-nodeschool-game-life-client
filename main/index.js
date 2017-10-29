b265fd2467cc443c56c9a437e9f56cea3c233c83
Dmitry Rusakov <jkikksa@gmail.com>
2017-10-29 19:04:25 +0300

    добавляет решение

--- main/index.js
+++ main/index.js
@@ -1,7 +1,63 @@
 'use strict';
 
-//
-// YOUR CODE GOES HERE...
+let socket = null;
+let game = null;
+const URL = `ws://127.0.0.1:3000/`;
+
+const initGame = (data) => {
+  game = new LifeGame(data.user, data.settings);
+  game.init();
+  game.setState(data.state);
+  game.send = (gameData) => {
+    socket.send(JSON.stringify({
+      type: `ADD_POINT`,
+      data: gameData
+    }));
+  };
+};
+
+const updateState = (data) => {
+  game.setState(data);
+};
+
+const typeToAction = {
+  INITIALIZE: initGame,
+  UPDATE_STATE: updateState
+};
+
+const parseData = (response) => {
+  try {
+    return JSON.parse(response.data);
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+const onConnectionOpen = () => {
+  console.log(`Connection is open`);
+};
+
+const onMessage = (evt) => {
+  const data = parseData(evt);
+  typeToAction[data.type](data.data);
+};
+
+const onConnectionError = () => {
+  console.log(`Connection error`);
+};
+
+const onConnectionClose = () => {
+  console.log(`Connection is close`);
+};
+
+App.onToken = (token) => {
+  socket = new WebSocket(`${URL}?token=${token}`);
+  socket.addEventListener(`open`, onConnectionOpen);
+  socket.addEventListener(`message`, onMessage);
+  socket.addEventListener(`error`, onConnectionError);
+  socket.addEventListener(`close`, onConnectionClose);
+};
+
 //
 // ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
 // ░░░░░░░░░░▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄░░░░░░░░░░░
